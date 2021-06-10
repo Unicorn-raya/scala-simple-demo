41d33e45413fb3b5c165f9c8995f1299d4490b82
unicorn-raya <ruizhang70@gmail.com>
2021-06-10 14:12:13 +0800

    add more ways of matching pattern

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
-object Add{
-  val op: String = "+"
+import scala.collection.mutable.Stack
+
+trait BinaryOp{
+  val op: String
   def apply(expr1: String, expr2: String) = expr1 + op + expr2
 
   def unapply(str: String): Option[(String,String)] = {
@@ -11,14 +13,42 @@ object Add{
   }
 }
 
+object Add extends {val op = "+"} with BinaryOp
+object Substract extends {val op = "-"} with BinaryOp
+object Mulitiply extends {val op = "*"} with BinaryOp
+object Divide extends {val op = "/"} with BinaryOp
+
 object Main extends App {
 
   def eval(str: String): Int = str match {
     case Add(expr1,expr2) => eval(expr1) + eval(expr2)
+    case Substract(expr1,expr2) => eval(expr1) + eval(expr2)
+    case Mulitiply(expr1,expr2) => eval(expr1) + eval(expr2)
+    case Divide(expr1,expr2) => eval(expr1) + eval(expr2)
     case _ => str.toInt
   }
+   
+  def matchBracket(str: String): Option[(Int,Int)] = {
+    val left = str.indexOf("(")
+    if (left > 0) {
+      val stack = Stack[Char]()
+      val remaining = str.substring(left + 1)
+      var index = 0
+      var right = 0
+      for (c <- remaining if right == 0) {
+        index += 1
+        c match {
+          case '(' => stack.push(c)
+          case ')' => if (stack.isEmpty) right = left + index else stack.pop()
+          case _ => 
+        }
+      }
+      Some(left,right)
+    }
+    else
+      None
+  }
 
-  println(eval("3+3+3"))
-  println(Add.apply("3","2")) 
-  println(Add.unapply("3+2+2"))
+  println(eval("4*3-2/2"))
+  println(matchBracket("1+2+(3*5)+3+3*(3+(3+5))"))
 }
